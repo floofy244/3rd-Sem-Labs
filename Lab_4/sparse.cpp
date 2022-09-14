4f53e7dfaa7b6aacf044179c1decf8f65c61a250
Aviral Malik <iamaviralmalik@gmail.com>
2022-09-14 13:42:58 +0530

    sparse matrix

---
+++ Lab_4/sparse.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class sparseMatrix
+{
+    int row, col, value;
+    int **arr;
+
+    public:
+
+    sparseMatrix(int r, int c, int v)
+    {
+        row = r;
+        col = c;
+        value = v;
+        arr = new int*[row];
+        for(int i = 0; i < row; i++)
+        {
+            arr[i] = new int[col];
+        }
+    } 
+
+    void input()
+    {
+        cout << "Enter the elements of the matrix: " << endl;
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < col; j++)
+            {
+                cin >> arr[i][j];
+            }
+        }
+    }
+
+    void display()
+    {
+        cout << "The matrix is: " << endl;
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < col; j++)
+            {
+                cout << arr[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    void sparse()
+    {
+        int count = 0;
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < col; j++)
+            {
+                if(arr[i][j] != 0)
+                {
+                    count++;
+                }
+            }
+        }
+        cout << "The number of non-zero elements are: " << count << endl;
+        sparseMatrix s(row, col, count);
+        int k = 0;
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < col; j++)
+            {
+                if(arr[i][j] != 0)
+                {
+                    s.arr[k][0] = i;
+                    s.arr[k][1] = j;
+                    s.arr[k][2] = arr[i][j];
+                    k++;
+                }
+            }
+        }
+        cout << "The sparse matrix is: " << endl;
+        for(int i = 0; i < count; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                cout << s.arr[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+
+};
+
+int main()
+{
+    
+}
