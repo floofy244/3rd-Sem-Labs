8f57ac3f39bc1eb5736719c5c4325b4611d9c7a7
Aviral Malik <iamaviralmalik@gmail.com>
2022-11-08 20:51:40 +0530

    lab 5

--- Lab_6/preeval.cpp
+++ Lab_6/preeval.cpp
@@ -1,80 +1,124 @@
-//Program to evaluate a Prefix Expression
+//Program to evaluate a Prefix Expression using character array
+
 #include <iostream>
-#include <algorithm>
-#include <cmath>
 using namespace std;
-const int STACK_SIZE=100;
-class Stack {
-    private:
+
+class Stack
+{
     int top;
-    char arr[STACK_SIZE];
-    public:
-    Stack(){
-        top=-1;
-    }
-    void push(int elem){
-        if(top<STACK_SIZE){
-            arr[++top]=elem;
-        }
-        else{
-            cout<<"Overflow"<<endl;
-        }
+    char *s;
+    int size;
+public:
+
+    Stack(int size)
+    {
+        this->size = size;
+        top = -1;
+        s = new char[size];
     }
 
-    int pop(){
-        if(top>=0)
-            return arr[top--];
-        cout<<"Stack Underflow!"<<endl;
-        return -1;
-    }
-
-    int peek(){
-        if(top>=0)
-            return arr[top];
-        else{
-            cout<<"Underflow"<<endl;
-            return -1;
-        }
-    }
-
-    void display(){
-        for(int i=top;i>=0;i--){
-            cout<<arr[i]<<endl;
-        }
-    }
+    void push(char x);
+    char pop();
+    int isFull();
+    int isEmpty();
+    char stackTop();
 };
 
-int Operator(char ch, int b, int a){
-    switch(ch){
-        case '*': return(a*b);break;
-        case '+': return(a+b);break;
-        case '-': return(a-b);break;
-        case '/': return(a/b);break;
-        case '$': return(pow(a,b));break;
-        case '^': return(pow(a,b));break;
-        default:
+void Stack::push(char x)
+{
+    if (isFull())
+        cout << "Stack Overflow" << endl;
+    else
+    {
+        top++;
+        s[top] = x;
+    }
+}
+
+char Stack::pop()
+{
+    char x = -1;
+    if (isEmpty())
+        cout << "Stack Underflow" << endl;
+    else
+    {
+        x = s[top];
+        top--;
+    }
+    return x;
+}
+
+int Stack::isFull()
+{
+    if (top == size - 1)
+        return 1;
+    return 0;
+}
+
+int Stack::isEmpty()
+{
+    if (top == -1)
+        return 1;
+    return 0;
+}
+
+char Stack::stackTop()
+{
+    if (isEmpty())
+        return -1;
+    return s[top];
+}
+
+int isOperand(char x)
+{
+    if (x == '+' || x == '-' || x == '*' || x == '/')
         return 0;
-    }
+    else
+        return 1;
 }
 
-int postEval(string post){
-    reverse(post.begin(),post.end());
-    Stack s;
-    int l=post.length();
-    for(int i=0;i<l;i++){
-        if(isdigit(post[i])){
-            s.push(post[i]-'0');
+int preEval(char *prefix)
+{
+    Stack stk(strlen(prefix));
+    int i = strlen(prefix) - 1;
+    int x1, x2, r;
+
+    while (i >= 0)
+    {
+        if (isOperand(prefix[i]))
+            stk.push(prefix[i] - '0');
+        else
+        {
+            x1 = stk.pop();
+            x2 = stk.pop();
+            switch (prefix[i])
+            {
+            case '+':
+                r = x1 + x2;
+                stk.push(r);
+                break;
+            case '-':
+                r = x1 - x2;
+                stk.push(r);
+                break;
+            case '*':
+                r = x1 * x2;
+                stk.push(r);
+                break;
+            case '/':
+                r = x1 / x2;
+                stk.push(r);
+                break;
+            }
         }
-        else{
-            s.push(Operator(post[i],s.pop(),s.pop()));
-        }
+        i--;
     }
-    return s.peek();
+    return stk.pop();
 }
 
-int main(){
-    string s;
-    cout<<"Enter expression:";
-    cin>>s;
-    cout<<postEval(s);
+int main()
+{
+    char *prefix = "-+7*45+20";
+    cout << "Result is " << preEval(prefix) << endl;
+    return 0;
 }
